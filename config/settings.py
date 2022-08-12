b646968460c98e51e2a3376f102d0070e6d25ab6
Naomi <surrealisticshadow@gmail.com>
2022-08-11 21:36:27 -0700

    installed and configured react. Configured connections between react and the db

--- config/settings.py
+++ config/settings.py
@@ -128,5 +128,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8000'
+    'http://localhost:3000',
+    'http://localhost:8000',
 ]
+
+CSRF_COOKIE_NAME = "csrftoken"
